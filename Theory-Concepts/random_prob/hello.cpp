03aeeec1eeaab3b0eb4fddc291addcf054facf5f
Nishant <mailittonishantkumar@gmail.com>
2021-05-18 14:53:58 +0530

    18-05-2021

--- Theory-Concepts/random_prob/hello.cpp
+++ Theory-Concepts/random_prob/hello.cpp
@@ -5,12 +5,12 @@ int main() {
     // your code goes here
     ios_base::sync_with_stdio(false);cin.tie(NULL); 
 
-    #ifndef ONLINE_JUDGE 
-    freopen("input.txt", "r", stdin); 
-    freopen("output.txt", "w", stdout);
-    #endif
-        int t=0;
-        cin>>t;
+    // #ifndef ONLINE_JUDGE 
+    // freopen("../input.txt", "r", stdin); 
+    // freopen("../output.txt", "w", stdout);
+    // #endif
+        int t=3;
+        // cin>>t;
         for(int i=t;i<=10 &t>0;t--)
         {
             cout<<"hello";
